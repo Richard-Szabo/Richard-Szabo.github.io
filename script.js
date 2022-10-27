aac04c4bb30ef341d9662aa11d93c73fea69003a
wield-zz <ricseee@gmail.com>
2022-10-27 17:14:21 +0200

    t2

--- script.js
+++
@@ -1,7 +0,0 @@
-// Navigation menu scripts
-document.querySelector('.menu-icon').addEventListener('click', () => {
-    document.querySelector('.navigation-menu').classList.toggle('active');
-    document.querySelectorAll('.menu-line').forEach((element) => {
-        element.classList.toggle('active');
-    })
-})
